53af58a41ef1bcb72d1170d3b9e730619f7d4819
ZZZank <3410764033@qq.com>
2023-12-29 19:30:27 +0800

    only remove bindings instead of fully remove ProbePlugin class

---
+++ src/main/java/com/prunoideae/probejs/plugin/ProbePlugin.java
@@ -0,0 +1,21 @@
+package com.prunoideae.probejs.plugin;
+
+import dev.latvian.kubejs.KubeJSPlugin;
+import dev.latvian.kubejs.event.IEventHandler;
+import dev.latvian.kubejs.script.BindingsEvent;
+import dev.latvian.kubejs.util.ListJS;
+
+public class ProbePlugin extends KubeJSPlugin {
+
+    private static Object onWrappedEvent(BindingsEvent event, Object[] args) {
+        for (Object o : ListJS.orSelf(args[0])) {
+            String e = String.valueOf(o);
+            event.type.manager.get().events.listen(e, new WrappedEventHandler(e, (IEventHandler) args[1]));
+        }
+        return null;
+    }
+
+    private static Object onWrappedForgeEvent(BindingsEvent event, Object[] args) {
+        return null;
+    }
+}
