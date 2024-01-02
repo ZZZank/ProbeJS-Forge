55a39197954ad599309212ed3bc3cecb89203c2a
ZZZank <3410764033@qq.com>
2024-01-02 18:45:44 +0800

    Handle common paths individually

---
+++ src/main/java/com/probejs/ProbePaths.java
@@ -0,0 +1,39 @@
+package com.probejs;
+
+import dev.latvian.kubejs.KubeJSPaths;
+import dev.latvian.kubejs.util.UtilsJS;
+
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+
+public class ProbePaths {
+
+    public static final Path PROBE = KubeJSPaths.DIRECTORY.resolve("probe");
+    public static final Path DOCS = PROBE.resolve("docs");
+    public static final Path GENERATED = PROBE.resolve("generated");
+    public static final Path USER_DEFINED = PROBE.resolve("user");
+    public static final Path SNIPPET = KubeJSPaths.DIRECTORY.resolve(".vscode");
+
+    public static void init() {
+        if (Files.notExists(PROBE, new LinkOption[0])) {
+            UtilsJS.tryIO(() -> Files.createDirectories(PROBE));
+        }
+        if (Files.notExists(DOCS, new LinkOption[0])) {
+            UtilsJS.tryIO(() -> Files.createDirectories(DOCS));
+        }
+        if (Files.notExists(GENERATED, new LinkOption[0])) {
+            UtilsJS.tryIO(() -> Files.createDirectories(GENERATED));
+        }
+        if (Files.notExists(USER_DEFINED, new LinkOption[0])) {
+            UtilsJS.tryIO(() -> Files.createDirectories(USER_DEFINED));
+        }
+        if (Files.notExists(SNIPPET, new LinkOption[0])) {
+            UtilsJS.tryIO(() -> Files.createDirectories(SNIPPET));
+        }
+    }
+
+    static {
+        init();
+    }
+}
