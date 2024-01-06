78a874cd94eded4a41a4a3edaa6f78dcd45df9df
ZZZank <3410764033@qq.com>
2024-01-06 22:03:45 +0800

    use our own ways for fetching data and formatting

--- src/main/java/com/probejs/recipe/RecipeHolders.java
+++ src/main/java/com/probejs/recipe/RecipeHolders.java
@@ -1,41 +1,72 @@
 package com.probejs.recipe;
 
-import com.probejs.formatter.formatter.FormatterClass;
+import com.mojang.datafixers.util.Pair;
+import com.probejs.ProbeJS;
+import com.probejs.formatter.NameResolver;
 import com.probejs.formatter.formatter.FormatterNamespace;
-import com.probejs.formatter.formatter.IFormatter;
-import com.probejs.info.ClassInfo;
+import com.probejs.util.PUtil;
 import dev.latvian.kubejs.recipe.RecipeTypeJS;
 import dev.latvian.kubejs.recipe.RegisterRecipeHandlersEvent;
 import dev.latvian.kubejs.util.KubeJSPlugins;
-import dev.latvian.mods.rhino.ast.FunctionNode.Form;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import java.util.Map.Entry;
 import net.minecraft.resources.ResourceLocation;
 
 public class RecipeHolders {
 
     static FormatterNamespace namespaced;
+    static Map<String, List<Pair<String, String>>> namespacedMap = new HashMap<>();
 
     public static void init() {
         Map<ResourceLocation, RecipeTypeJS> recipeHandlers = new HashMap<>();
         RegisterRecipeHandlersEvent event = new RegisterRecipeHandlersEvent(recipeHandlers);
         KubeJSPlugins.forEachPlugin(plugin -> plugin.addRecipes(event));
 
-        // RecipeTypeJS -> ClassInfo -> FormatterClass
-        List<FormatterClass> formatterClasses = recipeHandlers
-            .values()
-            .stream()
-            .map(value -> ClassInfo.getOrCache(value.factory.get().getClass()))
-            .map(FormatterClass::new)
-            .collect(Collectors.toList());
+        //TODO: remove when out of dev
+        int threshold = 10;
+        for (Entry<ResourceLocation, RecipeTypeJS> entry : recipeHandlers.entrySet()) {
+            if (threshold-- < 0) {
+                break;
+            }
+            ProbeJS.LOGGER.info(entry.getKey());
+            // ProbeJS.LOGGER.info(entry.getValue()); // same as getKey()
+            Class<?> clazz = entry.getValue().getClass();
+            ProbeJS.LOGGER.info(clazz);
 
-        namespaced = new FormatterNamespace("stub.probejs.recipeHolder", formatterClasses);
+            NameResolver.getResolvedName(clazz.getName()).getLastName();
+        }
+
+        recipeHandlers.forEach((key, value) -> {
+            String namespace = key.getNamespace();
+            String path = key.getPath();
+            String invokeName =
+                "Internal." + NameResolver.getResolvedName(value.getClass().getName()).getLastName();
+            
+            namespacedMap
+                .computeIfAbsent(namespace, k -> new ArrayList<Pair<String, String>>())
+                .add(new Pair<>(path, invokeName));
+        });
     }
 
     public static List<String> format(int indent, int stepIndent) {
-        return namespaced.format(indent, stepIndent);
+        List<String> formatted = new ArrayList<>();
+        // head
+        formatted.add("declare namespace stub.probejs.recipeHolder {");
+        // each class
+        for (Entry<String, List<Pair<String, String>>> entry : namespacedMap.entrySet()) {
+            formatted.add(String.format("    class %s {", entry.getKey()));
+            // each method inside class
+            for (Pair<String, String> pair : entry.getValue()) {
+                // we dont know how a Recipe Serializer actually works, so only `...args`
+                formatted.add(PUtil.indent(8) + pair.getFirst() + "(...args: object): " + pair.getSecond());
+            }
+            formatted.add("    }");
+        }
+        // end
+        formatted.add("}");
+        return formatted;
     }
 }
