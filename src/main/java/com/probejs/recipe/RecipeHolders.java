da47a532c942f2e6d0848774638d85f7b96f5d5e
ZZZank <3410764033@qq.com>
2024-01-07 19:38:29 +0800

    add a base class for indexing other recipeHolder classes

--- src/main/java/com/probejs/recipe/RecipeHolders.java
+++ src/main/java/com/probejs/recipe/RecipeHolders.java
@@ -2,7 +2,6 @@ package com.probejs.recipe;
 
 import com.mojang.datafixers.util.Pair;
 import com.probejs.formatter.NameResolver;
-import com.probejs.formatter.formatter.FormatterNamespace;
 import com.probejs.util.PUtil;
 import dev.latvian.kubejs.recipe.RecipeTypeJS;
 import java.util.ArrayList;
@@ -36,12 +35,22 @@ public class RecipeHolders {
     public static List<String> format(int indent, int stepIndent) {
         List<String> formatted = new ArrayList<>();
         // head
-        formatted.add(PUtil.indent(indent) + "declare namespace stub.probejs.recipeHolder {");
-        // each namespace
+        formatted.add(PUtil.indent(indent) + "declare namespace stub.probejs {");
         indent += stepIndent;
+        // a base class for indexing other classes
+        formatted.add(PUtil.indent(indent) + "class recipeHolder {");
+        indent += stepIndent;
+        for (String namespace : namespacedMap.keySet()) {
+            formatted.add(
+                PUtil.indent(indent) + String.format("readonly %s : stub.probejs.%s", namespace, namespace)
+            );
+        }
+        indent -= stepIndent;
+        formatted.add(PUtil.indent(indent) + "}");
+        // recipeHolder classes
         for (Entry<String, List<Pair<String, String>>> entry : namespacedMap.entrySet()) {
             formatted.add(String.format(PUtil.indent(indent) + "class %s {", entry.getKey()));
-            // each method inside namespace
+            // methods inside recipeHolder classes
             indent += stepIndent;
             for (Pair<String, String> pair : entry.getValue()) {
                 // we dont know how a Recipe Serializer actually works, so only `...args`
