68858102304d743a4b8f1ed69fc82ac7416cbb6f
ZZZank <3410764033@qq.com>
2024-03-19 10:25:32 +0800

    new RegistryInfo constructor, for Forge Registry

--- src/main/java/com/probejs/info/RegistryInfo.java
+++ src/main/java/com/probejs/info/RegistryInfo.java
@@ -5,17 +5,30 @@ import java.util.Set;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 public class RegistryInfo {
 
     public final Registry<?> raw;
+    public final ForgeRegistry<? extends IForgeRegistryEntry<?>> forgeRaw;
     public final ResourceKey<? extends Registry<?>> resKey;
     public final ResourceLocation parentId;
     public final ResourceLocation id;
     public final Set<ResourceLocation> names;
 
+    public RegistryInfo(ForgeRegistry<? extends IForgeRegistryEntry<?>> forgeRegistry) {
+        this.raw = null;
+        this.forgeRaw = forgeRegistry;
+        this.resKey = forgeRaw.getRegistryKey();
+        this.parentId = forgeRaw.getDefaultKey();
+        this.id = forgeRaw.getRegistryName();
+        this.names = forgeRaw.getKeys();
+    }
+
     public RegistryInfo(Registry<?> registry) {
         this.raw = registry;
+        this.forgeRaw = null;
         this.resKey = raw.key();
         this.parentId = resKey.getRegistryName();
         this.id = resKey.location();
@@ -24,6 +37,10 @@ public class RegistryInfo {
 
     public Registry<?> raw() {
         return this.raw;
+    }
+
+    public ForgeRegistry<? extends IForgeRegistryEntry<?>> forgeRaw() {
+        return this.forgeRaw;
     }
 
     public ResourceKey<? extends Registry<?>> resKey() {
