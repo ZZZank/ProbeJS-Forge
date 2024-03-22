b8b1b246af6f83d9926977629b1a8f1376421d26
ZZZank <3410764033@qq.com>
2024-03-23 00:24:36 +0800

    fetch KubeDump internally instead of relying on exported file

--- src/main/java/com/probejs/compiler/SnippetCompiler.java
+++ src/main/java/com/probejs/compiler/SnippetCompiler.java
@@ -5,16 +5,21 @@ import com.google.gson.JsonObject;
 import com.probejs.ProbeJS;
 import com.probejs.ProbePaths;
 import com.probejs.formatter.NameResolver;
-import dev.latvian.kubejs.KubeJSPaths;
+import dev.latvian.kubejs.KubeJSRegistries;
+import dev.latvian.kubejs.util.Tags;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagCollection;
 
 public class SnippetCompiler {
 
@@ -26,6 +31,59 @@ public class SnippetCompiler {
         public KubeDump(Map<String, Map<String, List<String>>> tags, Map<String, List<String>> registries) {
             this.tags = tags;
             this.registries = registries;
+        }
+
+        private static void putTag(
+            Map<String, Map<String, List<String>>> tags,
+            String type,
+            TagCollection<?> tagCollection
+        ) {
+            // tags.put(
+            //     type,
+            //     /* List<String> */
+            //     FormatterTag
+            //         .getTagsFromRegistry(tagCollection)
+            //         .stream()
+            //         .collect(Collectors.toMap(s -> s, s -> new ArrayList<>()))
+            // );
+            tags.put(
+                type,
+                tagCollection
+                    .getAvailableTags()
+                    .stream()
+                    .map(ResourceLocation::toString)
+                    .collect(Collectors.toMap(name -> name, name -> new ArrayList<>()))
+            );
+        }
+
+        private static <T> void putRegistry(
+            Map<String, List<String>> registries,
+            String type,
+            ResourceKey<Registry<T>> registry
+        ) {
+            registries.put(
+                type,
+                KubeJSRegistries
+                    .genericRegistry(registry)
+                    .getIds()
+                    .stream()
+                    .map(ResourceLocation::toString)
+                    .collect(Collectors.toList())
+            );
+        }
+
+        public static KubeDump fetch() {
+            Map<String, Map<String, List<String>>> tags = new HashMap<>();
+            Map<String, List<String>> registries = new HashMap<>();
+            putTag(tags, "items", Tags.items());
+            putTag(tags, "blocks", Tags.blocks());
+            putTag(tags, "fluids", Tags.fluids());
+            putTag(tags, "entity_types", Tags.entityTypes());
+            putRegistry(registries, "items", Registry.ITEM_REGISTRY);
+            putRegistry(registries, "blocks", Registry.BLOCK_REGISTRY);
+            putRegistry(registries, "fluids", Registry.FLUID_REGISTRY);
+            putRegistry(registries, "entity_types", Registry.ENTITY_TYPE_REGISTRY);
+            return new KubeDump(tags, registries);
         }
 
         @Override
@@ -100,14 +158,8 @@ public class SnippetCompiler {
     }
 
     private static void writeDumpSnippets() throws IOException {
-        Path kubePath = KubeJSPaths.EXPORTED.resolve("kubejs-server-export.json");
-        if (!kubePath.toFile().canRead()) {
-            ProbeJS.LOGGER.error("'kubejs-server-export.json' not found!");
-            return;
-        }
         Path codeFile = ProbePaths.SNIPPET.resolve("probe.code-snippets");
-        Reader reader = Files.newBufferedReader(kubePath);
-        KubeDump kubeDump = ProbeJS.GSON.fromJson(reader, KubeDump.class);
+        KubeDump kubeDump = KubeDump.fetch();
 
         BufferedWriter writer = Files.newBufferedWriter(codeFile);
         writer.write(ProbeJS.GSON.toJson(kubeDump.toSnippet()));
