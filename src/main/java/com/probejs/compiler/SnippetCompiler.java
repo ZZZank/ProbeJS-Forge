31d14822e7af2141126e2f42cce6af7f46e53ce8
ZZZank <3410764033@qq.com>
2024-03-24 10:20:35 +0800

    move KubeDump to a seperate class
    Ideally it will be shared by DataComplier(WIP yet) and SnippetComplier

--- src/main/java/com/probejs/compiler/SnippetCompiler.java
+++ src/main/java/com/probejs/compiler/SnippetCompiler.java
@@ -5,8 +5,7 @@ import com.google.gson.JsonObject;
 import com.probejs.ProbeJS;
 import com.probejs.ProbePaths;
 import com.probejs.formatter.NameResolver;
-import dev.latvian.kubejs.KubeJSRegistries;
-import dev.latvian.kubejs.util.Tags;
+import com.probejs.info.KubeDump;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,121 +15,65 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagCollection;
 
 public class SnippetCompiler {
 
-    public static class KubeDump {
-
-        public final Map<String, Collection<ResourceLocation>> tags;
-        public final Map<String, Collection<ResourceLocation>> registries;
-
-        public KubeDump(
-            Map<String, Collection<ResourceLocation>> tags,
-            Map<String, Collection<ResourceLocation>> registries
-        ) {
-            this.tags = tags;
-            this.registries = registries;
+    public static JsonObject toSnippet(KubeDump dump) {
+        JsonObject resultJson = new JsonObject();
+        // Compile normal entries to snippet
+        for (Map.Entry<String, Collection<ResourceLocation>> entry : dump.registries.entrySet()) {
+            String type = entry.getKey();
+            Map<String, List<String>> byModMembers = new HashMap<>();
+            entry
+                .getValue()
+                .forEach(rl ->
+                    byModMembers.computeIfAbsent(rl.getNamespace(), k -> new ArrayList<>()).add(rl.getPath())
+                );
+            byModMembers.forEach((mod, modMembers) -> {
+                JsonObject modMembersJson = new JsonObject();
+                JsonArray prefixes = new JsonArray();
+                if (ProbeJS.CONFIG.vanillaOrder) {
+                    prefixes.add(String.format("@%s.%s", mod, type));
+                } else {
+                    prefixes.add(String.format("@%s.%s", type, mod));
+                }
+                modMembersJson.add("prefix", prefixes);
+                modMembersJson.addProperty(
+                    "body",
+                    String.format("\"%s:${1|%s|}\"", mod, String.join(",", modMembers))
+                );
+                resultJson.add(String.format("%s_%s", type, mod), modMembersJson);
+            });
         }
 
-        private static void putTag(
-            Map<String, Collection<ResourceLocation>> target,
-            String type,
-            TagCollection<?> tagCollection
-        ) {
-            target.put(type, tagCollection.getAvailableTags());
+        // Compile tag entries to snippet
+        for (Map.Entry<String, Collection<ResourceLocation>> entry : dump.tags.entrySet()) {
+            String type = entry.getKey();
+            Map<String, List<String>> byModMembers = new HashMap<>();
+            entry
+                .getValue()
+                .forEach(rl ->
+                    byModMembers.computeIfAbsent(rl.getNamespace(), k -> new ArrayList<>()).add(rl.getPath())
+                );
+            byModMembers.forEach((mod, modMembers) -> {
+                JsonObject modMembersJson = new JsonObject();
+                JsonArray prefixes = new JsonArray();
+                if (ProbeJS.CONFIG.vanillaOrder) {
+                    prefixes.add(String.format("@%s.tags.%s", mod, type));
+                } else {
+                    prefixes.add(String.format("@%s.tags.%s", type, mod));
+                }
+                modMembersJson.add("prefix", prefixes);
+                modMembersJson.addProperty(
+                    "body",
+                    String.format("\"#%s:${1|%s|}\"", mod, String.join(",", modMembers))
+                );
+                resultJson.add(String.format("%s_tag_%s", type, mod), modMembersJson);
+            });
         }
 
-        private static <T> void putRegistry(
-            Map<String, Collection<ResourceLocation>> target,
-            String type,
-            ResourceKey<Registry<T>> registryKey
-        ) {
-            target.put(type, KubeJSRegistries.genericRegistry(registryKey).getIds());
-        }
-
-        public static KubeDump fetch() {
-            Map<String, Collection<ResourceLocation>> tags = new HashMap<>();
-            putTag(tags, "items", Tags.items());
-            putTag(tags, "blocks", Tags.blocks());
-            putTag(tags, "fluids", Tags.fluids());
-            putTag(tags, "entity_types", Tags.entityTypes());
-            Map<String, Collection<ResourceLocation>> registries = new HashMap<>();
-            putRegistry(registries, "items", Registry.ITEM_REGISTRY);
-            putRegistry(registries, "blocks", Registry.BLOCK_REGISTRY);
-            putRegistry(registries, "fluids", Registry.FLUID_REGISTRY);
-            putRegistry(registries, "entity_types", Registry.ENTITY_TYPE_REGISTRY);
-            return new KubeDump(tags, registries);
-        }
-
-        @Override
-        public String toString() {
-            return "KubeDump{" + "tags=" + tags + ", registries=" + registries + '}';
-        }
-
-        public JsonObject toSnippet() {
-            JsonObject resultJson = new JsonObject();
-            // Compile normal entries to snippet
-            for (Map.Entry<String, Collection<ResourceLocation>> entry : this.registries.entrySet()) {
-                String type = entry.getKey();
-                Map<String, List<String>> byModMembers = new HashMap<>();
-                entry
-                    .getValue()
-                    .forEach(rl ->
-                        byModMembers
-                            .computeIfAbsent(rl.getNamespace(), k -> new ArrayList<>())
-                            .add(rl.getPath())
-                    );
-                byModMembers.forEach((mod, modMembers) -> {
-                    JsonObject modMembersJson = new JsonObject();
-                    JsonArray prefixes = new JsonArray();
-                    if (ProbeJS.CONFIG.vanillaOrder) {
-                        prefixes.add(String.format("@%s.%s", mod, type));
-                    } else {
-                        prefixes.add(String.format("@%s.%s", type, mod));
-                    }
-                    modMembersJson.add("prefix", prefixes);
-                    modMembersJson.addProperty(
-                        "body",
-                        String.format("\"%s:${1|%s|}\"", mod, String.join(",", modMembers))
-                    );
-                    resultJson.add(String.format("%s_%s", type, mod), modMembersJson);
-                });
-            }
-
-            // Compile tag entries to snippet
-            for (Map.Entry<String, Collection<ResourceLocation>> entry : this.tags.entrySet()) {
-                String type = entry.getKey();
-                Map<String, List<String>> byModMembers = new HashMap<>();
-                entry
-                    .getValue()
-                    .forEach(rl ->
-                        byModMembers
-                            .computeIfAbsent(rl.getNamespace(), k -> new ArrayList<>())
-                            .add(rl.getPath())
-                    );
-                byModMembers.forEach((mod, modMembers) -> {
-                    JsonObject modMembersJson = new JsonObject();
-                    JsonArray prefixes = new JsonArray();
-                    if (ProbeJS.CONFIG.vanillaOrder) {
-                        prefixes.add(String.format("@%s.tags.%s", mod, type));
-                    } else {
-                        prefixes.add(String.format("@%s.tags.%s", type, mod));
-                    }
-                    modMembersJson.add("prefix", prefixes);
-                    modMembersJson.addProperty(
-                        "body",
-                        String.format("\"#%s:${1|%s|}\"", mod, String.join(",", modMembers))
-                    );
-                    resultJson.add(String.format("%s_tag_%s", type, mod), modMembersJson);
-                });
-            }
-
-            return resultJson;
-        }
+        return resultJson;
     }
 
     public static void compile() throws IOException {
@@ -145,7 +88,7 @@ public class SnippetCompiler {
         KubeDump kubeDump = KubeDump.fetch();
 
         BufferedWriter writer = Files.newBufferedWriter(codeFile);
-        writer.write(ProbeJS.GSON.toJson(kubeDump.toSnippet()));
+        writer.write(ProbeJS.GSON.toJson(toSnippet(kubeDump)));
         writer.flush();
         writer.close();
     }
---
+++ src/main/java/com/probejs/info/KubeDump.java
@@ -0,0 +1,61 @@
+package com.probejs.info;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.latvian.kubejs.KubeJSRegistries;
+import dev.latvian.kubejs.util.Tags;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagCollection;
+
+public class KubeDump {
+
+    public final Map<String, Collection<ResourceLocation>> tags;
+    public final Map<String, Collection<ResourceLocation>> registries;
+
+    public KubeDump(
+        Map<String, Collection<ResourceLocation>> tags,
+        Map<String, Collection<ResourceLocation>> registries
+    ) {
+        this.tags = tags;
+        this.registries = registries;
+    }
+
+    private static void putTag(
+        Map<String, Collection<ResourceLocation>> target,
+        String type,
+        TagCollection<?> tagCollection
+    ) {
+        target.put(type, tagCollection.getAvailableTags());
+    }
+
+    private static <T> void putRegistry(
+        Map<String, Collection<ResourceLocation>> target,
+        String type,
+        ResourceKey<Registry<T>> registryKey
+    ) {
+        target.put(type, KubeJSRegistries.genericRegistry(registryKey).getIds());
+    }
+
+    public static KubeDump fetch() {
+        Map<String, Collection<ResourceLocation>> tags = new HashMap<>();
+        putTag(tags, "items", Tags.items());
+        putTag(tags, "blocks", Tags.blocks());
+        putTag(tags, "fluids", Tags.fluids());
+        putTag(tags, "entity_types", Tags.entityTypes());
+        Map<String, Collection<ResourceLocation>> registries = new HashMap<>();
+        putRegistry(registries, "items", Registry.ITEM_REGISTRY);
+        putRegistry(registries, "blocks", Registry.BLOCK_REGISTRY);
+        putRegistry(registries, "fluids", Registry.FLUID_REGISTRY);
+        putRegistry(registries, "entity_types", Registry.ENTITY_TYPE_REGISTRY);
+        return new KubeDump(tags, registries);
+    }
+
+    @Override
+    public String toString() {
+        return "KubeDump{" + "tags=" + tags + ", registries=" + registries + '}';
+    }
+}
