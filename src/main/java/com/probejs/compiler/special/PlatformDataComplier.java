9889adeb9117c6ba235e7319116f611fefa6143f
ZZZank <3410764033@qq.com>
2024-03-27 19:25:09 +0800

    dump ids of loaded mods

---
+++ src/main/java/com/probejs/compiler/special/PlatformDataComplier.java
@@ -0,0 +1,29 @@
+package com.probejs.compiler.special;
+
+import com.probejs.ProbeJS;
+import com.probejs.formatter.formatter.FormatterNamespace;
+import com.probejs.formatter.formatter.FormatterRaw;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import me.shedaniel.architectury.platform.Platform;
+
+public class PlatformDataComplier {
+
+    public static void compile(BufferedWriter writer) throws IOException {
+        List<String> lines = new ArrayList<>();
+        //modids
+        String modids = Platform
+            .getModIds()
+            .stream()
+            .map(ProbeJS.GSON::toJson)
+            .collect(Collectors.joining("|"));
+        lines.add(String.format("type modids = %s;", modids));
+        lines.add("");
+
+        new FormatterNamespace("platform", Arrays.asList(new FormatterRaw(lines)));
+    }
+}
