8aab33f2de384eb5a61df56c013d1a65cf3cd61c
ZZZank <3410764033@qq.com>
2024-03-29 12:42:51 +0800

    rich complier, for items and item tags

---
+++ src/main/java/com/probejs/compiler/rich/item/ItemAttribute.java
@@ -0,0 +1,82 @@
+package com.probejs.compiler.rich.item;
+
+import com.google.gson.JsonObject;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.CropBlock;
+import org.jetbrains.annotations.Nullable;
+
+public class ItemAttribute {
+
+    private final ItemStack item;
+
+    public ItemAttribute(ItemStack item) {
+        this.item = item;
+    }
+
+    private String determineToolType() {
+        Item itemRepr = item.getItem();
+        if (itemRepr instanceof SwordItem) {
+            return "sword";
+        } else if (itemRepr instanceof PickaxeItem) {
+            return "pickaxe";
+        } else if (itemRepr instanceof ShovelItem) {
+            return "shovel";
+        } else if (itemRepr instanceof AxeItem) {
+            return "axe";
+        } else if (itemRepr instanceof HoeItem) {
+            return "hoe";
+        } else if (itemRepr instanceof ShearsItem) {
+            return "shears";
+        } else if (itemRepr instanceof TridentItem) {
+            return "trident";
+        } else if (itemRepr instanceof BowItem) {
+            return "bow";
+        } else if (itemRepr instanceof CrossbowItem) {
+            return "crossbow";
+        } else if (itemRepr instanceof ShieldItem) {
+            return "shield";
+        } else if (itemRepr instanceof ArmorItem) {
+            return "armor";
+        }
+        return null;
+    }
+
+    @Nullable
+    public JsonObject serialize() {
+        JsonObject prop;
+        //basic
+        Item itemRepr = item.getItem();
+        try {
+            prop = new JsonObject();
+            prop.addProperty("id", Item.getId(itemRepr));
+            prop.addProperty("localized", item.getHoverName().getString());
+            prop.addProperty("maxDamage", item.getMaxDamage());
+            prop.addProperty("maxStackSize", item.getMaxStackSize());
+        } catch (Throwable ignored) {
+            return null;
+        }
+        //tool
+        String toolType = determineToolType();
+        if (toolType != null) {
+            prop.addProperty("toolType", toolType);
+        }
+        //food
+        FoodProperties food = itemRepr.getFoodProperties();
+        if (food != null) {
+            JsonObject foodProp = new JsonObject();
+            foodProp.addProperty("nutrition", food.getNutrition());
+            foodProp.addProperty("saturation", food.getSaturationModifier());
+            foodProp.addProperty("alwaysEdible", food.canAlwaysEat());
+            prop.add("food", foodProp);
+        }
+        //block
+        if (itemRepr instanceof BlockItem) {
+            BlockItem blockItem = (BlockItem) itemRepr;
+            JsonObject blockProp = new JsonObject();
+            blockProp.addProperty("crop", blockItem.getBlock() instanceof CropBlock);
+            prop.add("block", blockProp);
+        }
+        return prop;
+    }
+}
---
+++ src/main/java/com/probejs/compiler/rich/item/ItemTagAttribute.java
@@ -0,0 +1,47 @@
+package com.probejs.compiler.rich.item;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import dev.latvian.kubejs.core.TagCollectionKJS;
+import dev.latvian.kubejs.util.Tags;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.Tag;
+import net.minecraft.tags.TagCollection;
+import net.minecraft.world.item.Item;
+
+public class ItemTagAttribute {
+
+    private final String id;
+    private final List<Item> itemsOfTag;
+
+    public ItemTagAttribute(Map.Entry<ResourceLocation, Tag<Item>> entry) {
+        this.id = entry.getKey().toString();
+        this.itemsOfTag = entry.getValue().getValues();
+    }
+
+    /*
+    public ItemTagAttribute(Tag<Item> itemTag) {
+        this.id = itemTag.location().toString();
+        this.itemsOfTag = new ArrayList<>();
+        Tags.items().getTag(this.id);
+        for (Holder<Item> holder : RegistryInfo.ITEM.getVanillaRegistry().getTagOrEmpty(itemTag)) {
+            itemsOfTag.add(holder.value());
+        }
+    }
+     */
+
+    public JsonObject serialize() {
+        JsonObject o = new JsonObject();
+        //tag id
+        o.addProperty("id", id);
+        //items in tag
+        JsonArray tags = new JsonArray();
+        itemsOfTag.stream().map(Registry.ITEM::getKey).map(ResourceLocation::toString).forEach(tags::add);
+        o.add("items", tags);
+        return o;
+    }
+}
