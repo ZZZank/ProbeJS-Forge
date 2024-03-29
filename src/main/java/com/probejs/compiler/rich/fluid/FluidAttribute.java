e3196ddbb6f37411559dd65d3dd57328bbb82e4f
ZZZank <3410764033@qq.com>
2024-03-29 12:58:40 +0800

    rich fluid compiling

--- src/main/java/com/probejs/compiler/rich/fluid/FluidAttribute.java
+++ src/main/java/com/probejs/compiler/rich/fluid/FluidAttribute.java
@@ -1,41 +1,49 @@
-package com.probejs.features.rich.fluid;
+package com.probejs.compiler.rich.fluid;
 
-import com.probejs.util.json.JObject;
-import com.probejs.util.json.JPrimitive;
-import dev.architectury.fluid.FluidStack;
-import dev.latvian.mods.kubejs.fluid.FluidStackJS;
-import dev.latvian.mods.kubejs.registry.RegistryInfo;
+import com.google.gson.JsonObject;
+import dev.latvian.kubejs.fluid.FluidStackJS;
+import java.util.Objects;
+import me.shedaniel.architectury.fluid.FluidStack;
+import net.minecraft.core.Registry;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Fluid;
 
-import java.util.Objects;
-
 public class FluidAttribute {
+
     private final Fluid fluid;
     private final FluidStack fluidStack;
-
 
     public FluidAttribute(Fluid fluid) {
         this.fluid = fluid;
         this.fluidStack = FluidStack.create(fluid, FluidStack.bucketAmount());
     }
 
-    public JObject serialize() {
+    public JsonObject serialize() {
+        JsonObject serialized = new JsonObject();
+        //id
+        serialized.addProperty("id", Objects.requireNonNull(Registry.FLUID.getKey(fluid)).toString());
+        //name
         String fluidName = "Unknown Fluid";
         try {
             fluidName = fluidStack.getName().getString();
         } catch (Exception e) {
             try {
                 fluidName = FluidStackJS.of(fluidStack).getId();
-            } catch (Exception ignored) {
-            }
+            } catch (Exception ignored) {}
         }
-        return JObject.create()
-                .add("id", JPrimitive.create(Objects.requireNonNull(RegistryInfo.FLUID.getVanillaRegistry().getKey(fluid)).toString()))
-                .add("localized", JPrimitive.create(fluidName))
-                .add("hasBucket", JPrimitive.create(fluid.getBucket() != Items.AIR))
-                .add("hasBlock", JPrimitive.create(fluid.defaultFluidState().createLegacyBlock().getBlock() != Blocks.AIR))
-                .add("bucketItem", JPrimitive.create(Objects.requireNonNull(RegistryInfo.ITEM.getVanillaRegistry().getKey(fluid.getBucket())).toString()));
+        serialized.addProperty("localized", fluidName);
+        //bucket or not
+        serialized.addProperty("hasBucket", fluid.getBucket() != Items.AIR);
+        serialized.addProperty(
+            "bucketItem",
+            Objects.requireNonNull(Registry.ITEM.getKey(fluid.getBucket())).toString()
+        );
+        //block or not
+        serialized.addProperty(
+            "hasBlock",
+            fluid.defaultFluidState().createLegacyBlock().getBlock() != Blocks.AIR
+        );
+        return serialized;
     }
 }
--- src/main/java/com/probejs/compiler/rich/fluid/RichFluidCompiler.java
+++ src/main/java/com/probejs/compiler/rich/fluid/RichFluidCompiler.java
@@ -1,34 +1,29 @@
-package com.probejs.features.rich.fluid;
+package com.probejs.compiler.rich.fluid;
 
-import com.mojang.blaze3d.pipeline.RenderTarget;
-import com.mojang.blaze3d.platform.NativeImage;
-import com.mojang.datafixers.util.Pair;
+import com.google.gson.JsonArray;
 import com.probejs.ProbeJS;
 import com.probejs.ProbePaths;
-import com.probejs.features.rich.ImageHelper;
-import com.probejs.util.json.JArray;
-import dev.latvian.mods.kubejs.registry.RegistryInfo;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.material.Fluid;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import net.minecraft.core.Registry;
 
 public class RichFluidCompiler {
+
     public static void compile() throws IOException {
-        JArray fluidArray = JArray.create()
-                .addAll(RegistryInfo.FLUID.entrySet().stream()
-                        .map(Map.Entry::getValue)
-                        .map(FluidAttribute::new)
-                        .map(FluidAttribute::serialize));
-        Path richFile = ProbePaths.WORKSPACE_SETTINGS.resolve("fluid-attributes.json");
+        JsonArray fluidArray = new JsonArray();
+        Registry.FLUID
+            .entrySet()
+            .stream()
+            .map(Map.Entry::getValue)
+            .map(FluidAttribute::new)
+            .map(FluidAttribute::serialize)
+            .forEach(fluidArray::add);
+        Path richFile = ProbePaths.WORKSPACE.resolve("fluid-attributes.json");
         BufferedWriter writer = Files.newBufferedWriter(richFile);
-        writer.write(ProbeJS.GSON.toJson(fluidArray.serialize()));
+        writer.write(ProbeJS.GSON.toJson(fluidArray));
         writer.close();
     }
 }
