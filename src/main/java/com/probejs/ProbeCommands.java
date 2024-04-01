a7450e3a8d68264a200c6ada7b9d0e50fe114929
ZZZank <3410764033@qq.com>
2024-04-01 20:06:51 +0800

    improve error message, making it red, with link to Github issue page

--- src/main/java/com/probejs/ProbeCommands.java
+++ src/main/java/com/probejs/ProbeCommands.java
@@ -17,8 +17,12 @@ import com.probejs.formatter.NameResolver;
 import dev.latvian.kubejs.KubeJSPaths;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.ClickEvent;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TextComponent;
 
 public class ProbeCommands {
@@ -52,14 +56,34 @@ public class ProbeCommands {
                                 RichLangCompiler.compile();
                             } catch (Exception e) {
                                 e.printStackTrace();
-                                sendSuccess(
-                                    context,
-                                    "[ERROR]Uncaught exception happened, terminating typing generation..."
-                                );
-                                sendSuccess(
-                                    context,
-                                    "Please report this error to ProbeJS Github with complete latest.log."
-                                );
+                                context
+                                    .getSource()
+                                    .sendSuccess(
+                                        new TextComponent(
+                                            "[ERROR]Uncaught exception happened, terminating typing generation..."
+                                        )
+                                            .withStyle(ChatFormatting.RED),
+                                        true
+                                    );
+                                MutableComponent githubLink = new TextComponent("ProbeJS Github")
+                                    .withStyle(
+                                        Style.EMPTY
+                                            .withUnderlined(true)
+                                            .withClickEvent(
+                                                new ClickEvent(
+                                                    ClickEvent.Action.OPEN_URL,
+                                                    "https://github.com/ZZZank/ProbeJS-Forge/issues"
+                                                )
+                                            )
+                                    );
+                                context
+                                    .getSource()
+                                    .sendSuccess(
+                                        new TextComponent("Please report this error to ")
+                                            .append(githubLink)
+                                            .append(" with complete latest.log."),
+                                        true
+                                    );
                             }
                             return sendSuccess(context, "ProbeJS typing generation finished.");
                         })
@@ -170,17 +194,14 @@ public class ProbeCommands {
     }
 
     /**
-     * @param context The command context, usually avaliable in Command.executes() callback
+     * @param source The command source, usually avaliable in Command.executes() callback,
+     * via {@code Command.executes(context -> context.getSource())}
      * @param message The message you want to send
-     * @param wantToInformAdmin
+     * @param allowLogging
      * @return Will always be `Command.SINGLE_SUCCESS`
      */
-    private static int sendSuccess(
-        CommandContext<CommandSourceStack> context,
-        String message,
-        boolean wantToInformAdmin
-    ) {
-        context.getSource().sendSuccess(new TextComponent(message), wantToInformAdmin);
+    private static int sendSuccess(CommandSourceStack source, String message, boolean allowLogging) {
+        source.sendSuccess(new TextComponent(message), allowLogging);
         return Command.SINGLE_SUCCESS;
     }
 
@@ -190,6 +211,6 @@ public class ProbeCommands {
      * @return Will always be `Command.SINGLE_SUCCESS`
      */
     private static int sendSuccess(CommandContext<CommandSourceStack> context, String message) {
-        return sendSuccess(context, message, false);
+        return sendSuccess(context.getSource(), message, true);
     }
 }
