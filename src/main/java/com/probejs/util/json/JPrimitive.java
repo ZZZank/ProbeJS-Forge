4b03b410f651d33f6b7cf014d3e00361b0f0073e
ZZZank <3410764033@qq.com>
2024-04-10 10:24:50 +0800

    json helpers

---
+++ src/main/java/com/probejs/util/json/JPrimitive.java
@@ -0,0 +1,51 @@
+package com.probejs.util.json;
+
+import com.google.gson.JsonPrimitive;
+
+public class JPrimitive implements IJsonBuilder<JsonPrimitive> {
+
+    public static JPrimitive of(Character value) {
+        return new JPrimitive(value);
+    }
+
+    public static JPrimitive of(String value) {
+        return new JPrimitive(value);
+    }
+
+    public static JPrimitive of(Number value) {
+        return new JPrimitive(value);
+    }
+
+    public static JPrimitive of(Boolean value) {
+        return new JPrimitive(value);
+    }
+
+    private final JsonPrimitive value;
+
+    public JPrimitive(Character value) {
+        this.value = new JsonPrimitive(value);
+    }
+
+    public JPrimitive(String value) {
+        this.value = new JsonPrimitive(value);
+    }
+
+    public JPrimitive(Number value) {
+        this.value = new JsonPrimitive(value);
+    }
+
+    public JPrimitive(boolean value) {
+        this.value = new JsonPrimitive(value);
+    }
+
+
+    @Override
+    public JsonPrimitive serialize() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return serialize().toString();
+    }
+}
