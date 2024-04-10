a398e70a88ef227f28ef94da469546a571dcc6ed
ZZZank <3410764033@qq.com>
2024-04-10 13:17:50 +0800

    implement class walking for generics in superclass/interfaces
    temp solution, ideally we should warp Class in Type, to allow unified processing

--- src/main/java/com/probejs/info/Walker.java
+++ src/main/java/com/probejs/info/Walker.java
@@ -4,6 +4,9 @@ import com.probejs.info.MethodInfo.ParamInfo;
 import com.probejs.info.type.ITypeInfo;
 import com.probejs.info.type.TypeInfoParameterized;
 import com.probejs.info.type.TypeInfoVariable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -16,39 +19,46 @@ public class Walker {
     private final boolean walkField;
     private final boolean walkSuper;
     private final boolean walkType;
+    private final boolean walkSuperGenerics;
 
     public Walker(
         Set<Class<?>> initial,
         boolean walkMethod,
         boolean walkField,
         boolean walkSuper,
-        boolean walkType
+        boolean walkType,
+        boolean walkSuperGenerics
     ) {
         this.initial = initial;
         this.walkMethod = walkMethod;
         this.walkField = walkField;
         this.walkSuper = walkSuper;
         this.walkType = walkType;
+        this.walkSuperGenerics = walkSuperGenerics;
     }
 
     public Walker(Set<Class<?>> initial) {
-        this(initial, true, true, true, true);
+        this(initial, true, true, true, true, true);
     }
 
     public Walker withWalkMethod(boolean walkMethod) {
-        return new Walker(initial, walkMethod, walkField, walkSuper, walkType);
+        return new Walker(initial, walkMethod, walkField, walkSuper, walkType, walkSuperGenerics);
     }
 
     public Walker withWalkField(boolean walkField) {
-        return new Walker(initial, walkMethod, walkField, walkSuper, walkType);
+        return new Walker(initial, walkMethod, walkField, walkSuper, walkType, walkSuperGenerics);
     }
 
     public Walker withWalkSuper(boolean walkSuper) {
-        return new Walker(initial, walkMethod, walkField, walkSuper, walkType);
+        return new Walker(initial, walkMethod, walkField, walkSuper, walkType, walkSuperGenerics);
     }
 
     public Walker withWalkType(boolean walkType) {
-        return new Walker(initial, walkMethod, walkField, walkSuper, walkType);
+        return new Walker(initial, walkMethod, walkField, walkSuper, walkType, walkSuperGenerics);
+    }
+
+    public Walker withWalkSuperGenerics(boolean walkSuperGenerics) {
+        return new Walker(initial, walkMethod, walkField, walkSuper, walkType, walkSuperGenerics);
     }
 
     private Set<Class<?>> walkType(ITypeInfo tInfo) {
@@ -70,6 +80,26 @@ public class Walker {
         Set<Class<?>> result = new HashSet<>();
         for (Class<?> clazz : classes) {
             ClassInfo info = ClassInfo.ofCache(clazz);
+
+            if (this.walkSuperGenerics) {
+                Type genericSuper = info.getClazzRaw().getGenericSuperclass();
+                if (genericSuper instanceof ParameterizedType) {
+                    Arrays
+                        .stream(((ParameterizedType) genericSuper).getActualTypeArguments())
+                        .filter(t -> t instanceof Class)
+                        .map(t -> (Class<?>) t)
+                        .forEach(result::add);
+                }
+                Arrays
+                    .stream(info.getClazzRaw().getGenericInterfaces())
+                    .filter(t -> t instanceof ParameterizedType)
+                    .map(t -> (ParameterizedType) t)
+                    .map(ParameterizedType::getActualTypeArguments)
+                    .flatMap(Arrays::stream)
+                    .filter(t -> t instanceof Class)
+                    .map(t -> (Class<?>) t)
+                    .forEach(result::add);
+            }
 
             if (walkSuper) {
                 ClassInfo superclass = info.getSuperClass();
