ac64a0d0c42e42ac6f0310498f9f1b85652501d6
ZZZank <3410764033@qq.com>
2024-05-02 21:43:01 +0800

    remove old manually assigned type alias

--- src/main/resources/docs/type_alias.d.ts
+++ src/main/resources/docs/type_alias.d.ts
@@ -1,6 +1,3 @@
-declare const Text = text;
-declare const Ingredient = ingredient;
-declare const Utils = utils;
-declare const Fluid = fluid;
+
 type SelfOrArray<T> = T | T[]
 type Unresolved = any
