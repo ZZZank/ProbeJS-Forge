e7d24c6780a6fd652fab6e09965a6a04d1c89eee
ZZZank <3410764033@qq.com>
2024-05-06 19:51:39 +0800

    new snippet format, not grouped by mod id

--- src/main/java/com/probejs/compiler/SnippetCompiler.java
+++ src/main/java/com/probejs/compiler/SnippetCompiler.java
@@ -1,7 +1,5 @@
 package com.probejs.compiler;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.probejs.ProbeJS;
@@ -13,81 +11,78 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import com.probejs.util.json.JArray;
+import com.probejs.util.json.JObject;
+import lombok.val;
 import net.minecraft.resources.ResourceLocation;
 
 public class SnippetCompiler {
 
     public static JsonObject toSnippet() {
-        JsonObject resultJson = new JsonObject();
+        val resultJson = JObject.of();
         // Compile normal entries to snippet
-        for (RegistryInfo info : SpecialData.computeRegistryInfos()) {
-            final String type = info.id.getPath();
-            final Map<String, List<String>> byModMembers = new HashMap<>();
-            info.names.forEach(rl ->
-                byModMembers.computeIfAbsent(rl.getNamespace(), k -> new ArrayList<>()).add(rl.getPath())
-            );
-            byModMembers.forEach((mod, modMembers) -> {
-                final JsonObject modMembersJson = new JsonObject();
-                //prefix
-                final JsonArray prefixes = new JsonArray();
-                if (ProbeJS.CONFIG.vanillaOrder) {
-                    prefixes.add(String.format("@%s.%s", mod, type));
-                } else {
-                    prefixes.add(String.format("@%s.%s", type, mod));
-                }
-                modMembersJson.add("prefix", prefixes);
-                //body
-                modMembersJson.addProperty(
-                    "body",
-                    String.format("\"%s:${1|%s|}\"", mod, String.join(",", modMembers))
-                );
-                //type as name, e.g. "fluid_minecraft"
-                resultJson.add(String.format("%s_%s", type, mod), modMembersJson);
-            });
-        }
+        generateRegistrySnippet(resultJson);
 
         // Compile tag entries to snippet
-        for (Map.Entry<String, Collection<ResourceLocation>> entry : SpecialData.computeTags().entrySet()) {
-            String type = entry.getKey();
-            Multimap<String, String> byModMembers = ArrayListMultimap.create();
-            entry
-                .getValue()
-                .forEach(rl ->
-                    byModMembers.put(rl.getNamespace(), rl.getPath())
-                );
-            byModMembers.asMap().forEach((mod, modMembers) -> {
-                final JsonObject modMembersJson = new JsonObject();
-                final JsonArray prefixes = new JsonArray();
-                if (ProbeJS.CONFIG.vanillaOrder) {
-                    prefixes.add(String.format("@%s.tags.%s", mod, type));
-                } else {
-                    prefixes.add(String.format("@%s.tags.%s", type, mod));
-                }
-                modMembersJson.add("prefix", prefixes);
-                modMembersJson.addProperty(
-                    "body",
-                    String.format("\"#%s:${1|%s|}\"", mod, String.join(",", modMembers))
-                );
-                resultJson.add(String.format("%s_tag_%s", type, mod), modMembersJson);
-            });
-        }
+        generateTagSnippet(resultJson);
 
-        return resultJson;
+        return resultJson.build();
+    }
+
+    private static void generateRegistrySnippet(JObject resultJson) {
+        val paths = new HashSet<String>();
+        for (RegistryInfo info : SpecialData.computeRegistryInfos()) {
+            String type = info.id.getPath();
+            if (paths.contains(type)) {
+                type = String.format("%s_%s", type, info.id.getNamespace());
+            } else {
+                paths.add(type);
+            }
+            resultJson.add(
+                type,
+                JObject.of()
+                    .add("prefix", JArray.of().add("@" + type))
+                    .add(
+                        "body",
+                        String.format(
+                            "\"${1|%s|}\"",
+                            info.names.stream().map(ResourceLocation::toString).collect(Collectors.joining(","))
+                        )
+                    )
+            );
+        }
+    }
+
+    private static void generateTagSnippet(JObject resultJson) {
+        for (val entry : SpecialData.computeTags().entrySet()) {
+            String type = String.format("%s_tag", entry.getKey());
+            resultJson.add(
+                type,
+                JObject.of()
+                    .add("prefix", JArray.of().add("@" + type))
+                    .add(
+                        "body",
+                        String.format(
+                            "\"#${1|%s|}\"",
+                            entry.getValue().stream().map(ResourceLocation::toString).collect(Collectors.joining(","))
+                        )
+                    )
+            );
+        }
     }
 
     public static void compile() throws IOException {
         if (ProbeJS.CONFIG.exportClassNames) {
             compileClassNames();
         }
-        Path codeFile = ProbePaths.WORKSPACE.resolve("probe.code-snippets");
-        BufferedWriter writer = Files.newBufferedWriter(codeFile);
+        val codeFile = ProbePaths.WORKSPACE.resolve("probe.code-snippets");
+        val writer = Files.newBufferedWriter(codeFile);
 
-        ProbeJS.GSON.toJson(toSnippet(), writer);
+        val snippet = toSnippet();
+        ProbeJS.GSON.toJson(snippet, writer);
 
         writer.close();
     }
