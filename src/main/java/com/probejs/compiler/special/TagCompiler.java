1ce261fe6f9c94a62782f9b689e6010070c06b34
ZZZank <3410764033@qq.com>
2024-05-10 19:54:15 +0800

    TagCompiler, adapt to SpecialData change

--- src/main/java/com/probejs/compiler/special/TagCompiler.java
+++ src/main/java/com/probejs/compiler/special/TagCompiler.java
@@ -5,36 +5,41 @@ import com.probejs.formatter.FormatterNamespace;
 import com.probejs.formatter.FormatterRaw;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
+
+import lombok.val;
 import net.minecraft.resources.ResourceLocation;
 
 public abstract class TagCompiler {
 
-    private static Map<String, Collection<ResourceLocation>> tags;
+    private static Map<ResourceLocation, Collection<ResourceLocation>> tags;
 
-    public static void init(Map<String, Collection<ResourceLocation>> tags) {
+    public static void init(Map<ResourceLocation, Collection<ResourceLocation>> tags) {
         TagCompiler.tags = tags;
     }
 
     public static List<String> format(int indent, int stepIndent) {
-        List<String> lines = tags
-            .entrySet()
-            .stream()
-            .map(e ->
-                String.format(
-                    "type %s = %s",
-                    e.getKey(),
-                    e
-                        .getValue()
-                        .stream()
-                        .map(rl -> ProbeJS.GSON.toJson(rl.toString()))
-                        .collect(Collectors.joining("|"))
-                )
-            )
-            .collect(Collectors.toList());
+        val lines = new ArrayList<String>();
+        val duped = new HashSet<String>();
+        tags.forEach((id, entriesRl) -> {
+            //name
+            String name = id.getPath();
+            if (duped.contains(name)) {
+                name = name + "_" + id.getNamespace();
+            }
+            duped.add(name);
+            //entries
+            String entries = entriesRl.stream()
+                .map(ResourceLocation::toString)
+                .map(ProbeJS.GSON::toJson)
+                .collect(Collectors.joining("|"));
+            if (entries.isEmpty()) {
+                entries = "never";
+            }
+            //link together
+            lines.add(String.format("type %s = %s;", name, entries));
+        });
         return new FormatterNamespace("Tag", new FormatterRaw(lines, false))
             .formatLines(indent, stepIndent);
     }
