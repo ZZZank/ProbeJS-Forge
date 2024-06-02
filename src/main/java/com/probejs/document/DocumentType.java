06e2d7ecc5d8ddf8b6f60374dff67c154e8a9bf7
ZZZank <3410764033@qq.com>
2024-06-02 17:06:00 +0800

    make DocumentType include comments when formatting

--- src/main/java/com/probejs/document/DocumentType.java
+++ src/main/java/com/probejs/document/DocumentType.java
@@ -6,20 +6,20 @@ import com.probejs.document.type.DocType;
 import com.probejs.document.type.DocTypeResolver;
 import com.probejs.formatter.api.MultiFormatter;
 import com.probejs.util.PUtil;
-import com.probejs.util.Pair;
 import com.probejs.util.StringUtil;
 import lombok.Getter;
+import lombok.val;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * {@code type <name> = <type>;}
+ */
 @Getter
-public class DocumentType implements IDocumentProvider<DocumentType>, MultiFormatter, IConcrete {
+public class DocumentType extends DocumentProperty implements IDocumentProvider<DocumentType>, MultiFormatter {
 
-    //type <name> = <type>;
-
-    private DocumentComment comment;
     private final String name;
     private final DocType type;
 
@@ -33,7 +33,7 @@ public class DocumentType implements IDocumentProvider<DocumentType>, MultiForma
         if (line.endsWith(";")) {
             line = line.substring(0, line.length() - 1);
         }
-        Pair<String, String> nameType = StringUtil.splitFirst(line, "=");
+        val nameType = StringUtil.splitFirst(line, "=");
         return new DocumentType(nameType.first().trim(), DocTypeResolver.resolve(nameType.second().trim()));
     }
 
@@ -45,25 +45,15 @@ public class DocumentType implements IDocumentProvider<DocumentType>, MultiForma
     @Override
     public List<String> formatLines(int indent, int stepIndent) {
         if (!CommentUtil.isLoaded(comment) || CommentUtil.isHidden(comment)) {
-            return new ArrayList<>(0);
+            return Collections.emptyList();
         }
-        return Collections.singletonList(
-            String.format(
-                "%stype %s = %s;",
-                PUtil.indent(indent),
-                name,
-                DocType.defaultTransformer.apply(type, type.getTypeName())
-            )
-        );
+        val lines = new ArrayList<String>(1);
+        lines.addAll(comment.formatLines(0, 4));
+        lines.add(String.format("%stype %s = %s;",
+            PUtil.indent(indent),
+            name,
+            type.transform(DocType.defaultTransformer)
+        ));
+        return lines;
     }
-
-    @Override
-    public void acceptDeco(List<IDecorative> decorates) {
-        decorates.forEach(d -> {
-            if (d instanceof DocumentComment) {
-                comment = (DocumentComment) d;
-            }
-        });
-    }
-
 }
