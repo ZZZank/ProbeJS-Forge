aac151933c6e687d3a62858ad0ef6c889d90689b
ZZZank <3410764033@qq.com>
2024-08-04 16:33:53 +0800

    IntegratedDynamics recipe doc

---
+++ src/main/java/zzzank/probejs/docs/recipes/IntegratedDynamics.java
@@ -0,0 +1,29 @@
+package zzzank.probejs.docs.recipes;
+
+import dev.latvian.kubejs.recipe.mod.IDSqueezerRecipeJS;
+import zzzank.probejs.lang.typescript.ScriptDump;
+import zzzank.probejs.lang.typescript.code.type.Types;
+
+import static zzzank.probejs.docs.recipes.BuiltinRecipeDocs.*;
+
+/**
+ * @author ZZZank
+ */
+class IntegratedDynamics extends RecipeDocProvider {
+    @Override
+    public void addDocs(ScriptDump scriptDump) {
+        add(
+            "squeezer",
+            recipeFn().outputs(STACK_N).input(INGR).returnType(Types.type(IDSqueezerRecipeJS.class))
+        );
+        add(
+            "mechanical_squeezer",
+            recipeFn().outputs(STACK_N).input(INGR).returnType(Types.type(IDSqueezerRecipeJS.class))
+        );
+    }
+
+    @Override
+    public String namespace() {
+        return "integrateddynamics";
+    }
+}
