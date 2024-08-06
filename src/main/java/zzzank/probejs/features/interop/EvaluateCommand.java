688beb590ba5d348c5fc745c5f63840e335475ff
ZZZank <3410764033@qq.com>
2024-08-06 20:11:34 +0800

    impl `evaluate` command

--- src/main/java/zzzank/probejs/features/interop/EvaluateCommand.java
+++ src/main/java/zzzank/probejs/features/interop/EvaluateCommand.java
@@ -1,12 +1,16 @@
 package zzzank.probejs.features.interop;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 import dev.latvian.kubejs.KubeJS;
 import dev.latvian.kubejs.script.ScriptManager;
 import dev.latvian.kubejs.server.ServerScriptManager;
+import dev.latvian.mods.rhino.NativeJavaObject;
 import lombok.val;
 import zzzank.probejs.features.bridge.Command;
+import zzzank.probejs.utils.JsonUtils;
 
 public class EvaluateCommand extends Command {
     @Override
@@ -19,30 +23,23 @@ public class EvaluateCommand extends Command {
         val scriptType = payload.get("scriptType").getAsString();
         val content = payload.get("content").getAsString();
 
-        ScriptManager scriptManager;
-        if (scriptType.equals("startup_scripts")) {
-            scriptManager = KubeJS.startupScriptManager;
-        } else if (scriptType.equals("client_scripts")) {
-            scriptManager = KubeJS.clientScriptManager;
-        } else if (scriptType.equals("server_scripts")) {
-            scriptManager = ServerScriptManager.instance.scriptManager;
-        } else {
-            scriptManager = null;
-        }
+        ScriptManager scriptManager = switch (scriptType) {
+            case "startup_scripts" -> KubeJS.startupScriptManager;
+            case "client_scripts" -> KubeJS.clientScriptManager;
+            case "server_scripts" -> ServerScriptManager.instance.scriptManager;
+            default -> throw new RuntimeException("Unable to get script manager.");
+        };
 
-        if (scriptManager == null) {
-            throw new RuntimeException("Unable to get script manager.");
+        //well, there's one and only one ScriptPack in so-called `scriptManager.packs`
+        val pack = scriptManager.packs.values().stream().findAny().get();
+        Object result = pack.context.evaluateString(pack.scope, content, "probejsEvaluator", 1, null);
+        if (result instanceof NativeJavaObject nativeJavaObject) {
+            result = nativeJavaObject.unwrap();
         }
-        throw new RuntimeException("Unsupported operation");
-//        Context context = scriptManager.contextFactory.enter();
-//        Object result = context.evaluateString(context.topLevelScope, content, "probejsEvaluator", 1, null);
-//        if (result instanceof NativeJavaObject nativeJavaObject) {
-//            result = nativeJavaObject.unwrap();
-//        }
-//        JsonElement jsonElement = JsonUtils.parseObject(result);
-//        if (jsonElement == JsonNull.INSTANCE && result != null) {
-//            jsonElement = new JsonPrimitive(result.toString());
-//        }
-//        return jsonElement;
+        JsonElement jsonElement = JsonUtils.parseObject(result);
+        if (jsonElement == JsonNull.INSTANCE && result != null) {
+            jsonElement = new JsonPrimitive(result.toString());
+        }
+        return jsonElement;
     }
 }
