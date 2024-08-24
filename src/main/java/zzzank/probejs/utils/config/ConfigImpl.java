e667e5cd3127a4e90095670331587775963787dc
ZZZank <3410764033@qq.com>
2024-08-25 01:38:18 +0800

    file io for new config system
    todo: config auto-update

--- src/main/java/zzzank/probejs/utils/config/ConfigImpl.java
+++ src/main/java/zzzank/probejs/utils/config/ConfigImpl.java
@@ -2,9 +2,11 @@ package zzzank.probejs.utils.config;
 
 import com.google.common.collect.HashBasedTable;
 import com.google.common.collect.Table;
+import com.google.gson.JsonObject;
 import lombok.val;
 import zzzank.probejs.ProbeJS;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
@@ -13,19 +15,44 @@ import java.nio.file.Path;
 public class ConfigImpl {
 
     public final String defaultNamespace;
+    public final ConfigEntrySerde serde;
     private final Table<String, String, ConfigEntry<?>> all;
     private final Path path;
 
     public ConfigImpl(Path path, String defaultNamespace) {
         this.path = path;
+        this.serde = new ConfigEntrySerde(this);
         all = HashBasedTable.create();
         this.defaultNamespace = defaultNamespace;
     }
 
     public void initFromFile() {
+        try (val reader = Files.newBufferedReader(path)) {
+            val object = ProbeJS.GSON.fromJson(reader, JsonObject.class);
+            for (val entry : object.entrySet()) {
+                val configEntry = serde.fromJson(entry.getKey(), entry.getValue().getAsJsonObject());
+                this.put(configEntry);
+            }
+        } catch (Exception e) {
+            ProbeJS.LOGGER.error("Error happened when reading configs from file", e);
+        }
     }
 
     public void save() {
+        try (val writer = Files.newBufferedWriter(path)) {
+            val object = new JsonObject();
+            for (val cell : all.cellSet()) {
+                val configEntry = cell.getValue();
+                if (configEntry == null) {
+                    continue;
+                }
+                val pair = serde.toJson(configEntry);
+                object.add(pair.getFirst(), pair.getSecond());
+            }
+            ProbeJS.GSON_WRITER.toJson(object, writer);
+        } catch (Exception e) {
+            ProbeJS.LOGGER.error("Error happened when writing configs to file", e);
+        }
     }
 
     public ConfigEntry<?> get(String name) {
@@ -40,5 +67,12 @@ public class ConfigImpl {
         val configEntry = builder.build(this);
         all.put(builder.namespace, builder.name, configEntry);
         return configEntry;
+    }
+
+    /**
+     * @return the value previously associated with the keys, or null if no mapping existed for the keys
+     */
+    public ConfigEntry<?> put(ConfigEntry<?> configEntry) {
+        return all.put(configEntry.namespace, configEntry.name, configEntry);
     }
 }
