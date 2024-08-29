80dcb5a204532ae8fcd5c13467f773a2f39f82c2
ZZZank <3410764033@qq.com>
2024-08-29 14:06:17 +0800

    smarter scope wrapping

--- src/main/java/zzzank/probejs/features/kubejs/ScriptTransformer.java
+++ src/main/java/zzzank/probejs/features/kubejs/ScriptTransformer.java
@@ -5,6 +5,7 @@ import dev.latvian.mods.rhino.Context;
 import dev.latvian.mods.rhino.Parser;
 import dev.latvian.mods.rhino.ast.*;
 import lombok.val;
+import zzzank.probejs.utils.GameUtils;
 import zzzank.probejs.utils.NameUtils;
 import zzzank.probejs.ProbeConfig;
 
@@ -23,10 +24,12 @@ public class ScriptTransformer {
     };
 
     public final Set<String> exportedSymbols;
+    private int requireCounts;
     public List<String> lines;
 
     public ScriptTransformer(String[] lines) {
         this.lines = Arrays.asList(lines);
+        requireCounts = 0;
         this.exportedSymbols = new HashSet<>();
     }
 
@@ -45,32 +48,34 @@ public class ScriptTransformer {
             for (val variable : variables) {
                 // used require()
                 if (
-                    variable.getInitializer() instanceof FunctionCall call
-                    && call.getTarget() instanceof Name name
-                    && name.getIdentifier().equals("require")
+                    !(variable.getInitializer() instanceof FunctionCall call)
+                        || !(call.getTarget() instanceof Name name)
+                        || !name.getIdentifier().equals("require")
                 ) {
-                    val loaded = call.getArguments().get(0);
-                    if (!(loaded instanceof StringLiteral literal)) {
-                        //well, that should not happen, `require()` is no supposed to be used for handling non-string object
-                        continue;
+                    continue;
+                }
+                val loaded = call.getArguments().get(0);
+                if (!(loaded instanceof StringLiteral literal)) {
+                    //well, that should not happen, `require()` is no supposed to be used for handling non-string object
+                    continue;
+                }
+                requireCounts++;
+                // is java package, transform if it's const
+                if (literal.getValue().startsWith("packages/")) {
+                    if (declaration.isConst()) {
+                        joined = NameUtils.replaceRegion(
+                            joined,
+                            statement.getPosition(),
+                            statement.getPosition() + statement.getLength(),
+                            "const ",
+                            PLACEHOLDER
+                        );
                     }
-                    // is java package, transform if it's const
-                    if (literal.getValue().startsWith("packages/")) {
-                        if (declaration.isConst()) {
-                            joined = NameUtils.replaceRegion(
-                                joined,
-                                statement.getPosition(),
-                                statement.getPosition() + statement.getLength(),
-                                "const ",
-                                PLACEHOLDER
-                            );
-                        }
-                    } else {
-                        // not java package, cut it
-                        cuts.add(new Integer[]{
-                            statement.getPosition(), statement.getPosition() + statement.getLength()
-                        });
-                    }
+                } else {
+                    // not java package, cut it
+                    cuts.add(new Integer[]{
+                        statement.getPosition(), statement.getPosition() + statement.getLength()
+                    });
                 }
             }
         }
@@ -107,11 +112,11 @@ public class ScriptTransformer {
 
     // Wraps the code in let {...} = (()=>{...;return {...};})()
     public void wrapScope() {
-        String exported = exportedSymbols.stream()
+        val exported = exportedSymbols
+            .stream()
             .map(s -> String.format("%s: %s", s, s))
             .collect(Collectors.joining(", "));
-        String destructed = String.join(", ", exportedSymbols);
-        lines.set(0, String.format("const {%s} = (()=>{ %s", destructed, lines.get(0)));
+        lines.set(0, String.format("const {%s} = (()=>{ %s", exported, lines.get(0)));
         lines.set(lines.size() - 1, String.format("%s; return {%s};})()", lines.get(lines.size() - 1), exported));
     }
 
@@ -119,12 +124,12 @@ public class ScriptTransformer {
         try {
             processExport();
             processRequire();
-            // If there's no symbol to be exported, it will still be wrapped, for consistency
-            if (ProbeConfig.isolatedScopes.get()) {
+            // If there's no symbol to be exported, and no `require()` call, it will not be marked as CommonJS module
+            if (ProbeConfig.isolatedScopes.get() && (!exportedSymbols.isEmpty() || requireCounts != 0)) {
                 wrapScope();
             }
         } catch (Throwable t) {
-            t.printStackTrace();
+            GameUtils.logThrowable(t);
         }
 
         return lines.toArray(new String[0]);
