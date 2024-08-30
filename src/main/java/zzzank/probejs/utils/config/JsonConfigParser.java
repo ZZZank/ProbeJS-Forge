fa962c04e660914045adb68267598858f7792719
ZZZank <3410764033@qq.com>
2024-08-31 00:25:30 +0800

    parser for new config version

--- src/main/java/zzzank/probejs/utils/config/JsonConfigParser.java
+++ src/main/java/zzzank/probejs/utils/config/JsonConfigParser.java
@@ -2,6 +2,8 @@ package zzzank.probejs.utils.config;
 
 import com.google.gson.JsonObject;
 import lombok.val;
+import zzzank.probejs.ProbeConfig;
+import zzzank.probejs.ProbeJS;
 import zzzank.probejs.utils.CollectUtils;
 import zzzank.probejs.utils.JsonUtils;
 
@@ -13,8 +15,9 @@ import java.util.List;
 public interface JsonConfigParser {
 
     List<JsonConfigParser> REGISTERED = CollectUtils.ofList(
-        new OldParser(),
-        new DefaultParser()
+        new ParserOld(),
+        new ParserVersion2(),
+        new ParserVersion3()
     );
 
     static JsonConfigParser select(JsonObject rawConfig) {
@@ -30,7 +33,7 @@ public interface JsonConfigParser {
 
     void parse(ConfigImpl source, JsonObject rawConfig);
 
-    class OldParser implements JsonConfigParser {
+    class ParserOld implements JsonConfigParser {
 
         @Override
         public boolean test(JsonObject rawConfig) {
@@ -48,16 +51,36 @@ public interface JsonConfigParser {
         }
     }
 
-    class DefaultParser implements JsonConfigParser {
+    class ParserVersion2 implements JsonConfigParser {
 
         @Override
         public boolean test(JsonObject rawConfig) {
-            return true;
+            return rawConfig.has(ConfigImpl.VERSION_KEY);
         }
 
         @Override
         public void parse(ConfigImpl source, JsonObject rawConfig) {
             rawConfig.remove(ConfigImpl.VERSION_KEY);
+            for (val entry : rawConfig.entrySet()) {
+                val configEntry = source.serde.fromJson(entry.getKey(), entry.getValue().getAsJsonObject());
+                if (configEntry == null) {
+                    continue;
+                }
+                source.merge(configEntry);
+            }
+        }
+    }
+
+    class ParserVersion3 implements JsonConfigParser {
+
+        @Override
+        public boolean test(JsonObject rawConfig) {
+            val cfg = rawConfig.get(ProbeJS.MOD_ID + '.' + ProbeConfig.configVersion.name);
+            return cfg != null && cfg.getAsJsonObject().get(ConfigEntrySerde.VALUE_KEY).getAsInt() == 3;
+        }
+
+        @Override
+        public void parse(ConfigImpl source, JsonObject rawConfig) {
             for (val entry : rawConfig.entrySet()) {
                 val configEntry = source.serde.fromJson(entry.getKey(), entry.getValue().getAsJsonObject());
                 if (configEntry == null) {
