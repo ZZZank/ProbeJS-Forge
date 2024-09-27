9aeee692cbcf0877e4e6f8e723dd7a73e64cb0bf
ZZZank <3410764033@qq.com>
2024-09-27 14:30:55 +0800

    fix ClassData accessing

--- src/main/java/zzzank/probejs/features/forge_scan/AccessClassData.java
+++ src/main/java/zzzank/probejs/features/forge_scan/AccessClassData.java
@@ -1,9 +1,10 @@
 package zzzank.probejs.features.forge_scan;
 
-import lombok.SneakyThrows;
 import lombok.val;
 import net.minecraftforge.forgespi.language.ModFileScanData;
 import org.objectweb.asm.Type;
+import zzzank.probejs.ProbeJS;
+import zzzank.probejs.utils.GameUtils;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
@@ -22,10 +23,18 @@ public class AccessClassData {
         val lookup = MethodHandles.lookup();
         val c = ModFileScanData.ClassData.class;
         try {
-            accessClazz = lookup.findGetter(c, "clazz", Type.class);
-            accessParent = lookup.findGetter(c, "parent", Type.class);
-            accessInterfaces = lookup.findGetter(c, "interfaces", Set.class);
-        } catch (NoSuchFieldException | IllegalAccessException ignored) {
+            var f = c.getDeclaredField("clazz");
+            f.setAccessible(true);
+            accessClazz = lookup.unreflectGetter(f);
+            f = c.getDeclaredField("parent");
+            f.setAccessible(true);
+            accessParent = lookup.unreflectGetter(f);
+            f = c.getDeclaredField("interfaces");
+            f.setAccessible(true);
+            accessInterfaces = lookup.unreflectGetter(f);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            ProbeJS.LOGGER.error("accessing '{}' failed", ModFileScanData.ClassData.class);
+            GameUtils.logThrowable(e);
             throw new IllegalStateException();
         }
     }
@@ -36,16 +45,28 @@ public class AccessClassData {
         this.raw = raw;
     }
 
-    @SneakyThrows
+    //    @SneakyThrows
     public Type pjs$clazz() {
-        return (Type) accessClazz.invoke(raw);
+        try {
+            return (Type) accessClazz.invoke(raw);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
-    @SneakyThrows
+
     public Type pjs$parent() {
-        return (Type) accessParent.invoke(raw);
+        try {
+            return (Type) accessParent.invoke(raw);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
-    @SneakyThrows
+
     public Set<Type> pjs$interfaces() {
-        return (Set<Type>) accessInterfaces.invoke(raw);
+        try {
+            return (Set<Type>) accessInterfaces.invoke(raw);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
     }
 }
