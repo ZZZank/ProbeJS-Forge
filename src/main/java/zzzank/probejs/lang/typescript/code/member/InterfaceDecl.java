d4fc78ababfcec55bcf631fa82c5d528b0ddcbb0
ZZZank <3410764033@qq.com>
2024-09-30 10:37:56 +0800

    add SomeInterface$$Static, refactor interface code

--- src/main/java/zzzank/probejs/lang/typescript/code/member/InterfaceDecl.java
+++ src/main/java/zzzank/probejs/lang/typescript/code/member/InterfaceDecl.java
@@ -1,5 +1,6 @@
 package zzzank.probejs.lang.typescript.code.member;
 
+import lombok.val;
 import org.jetbrains.annotations.Nullable;
 import zzzank.probejs.lang.typescript.Declaration;
 import zzzank.probejs.lang.typescript.code.Code;
@@ -8,17 +9,50 @@ import zzzank.probejs.lang.typescript.code.ts.Wrapped;
 import zzzank.probejs.lang.typescript.code.type.BaseType;
 import zzzank.probejs.lang.typescript.code.type.TSVariableType;
 import zzzank.probejs.lang.typescript.code.type.Types;
+import zzzank.probejs.lang.typescript.refer.ImportType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InterfaceDecl extends ClassDecl {
-    private final Wrapped.Namespace namespace;
 
     public InterfaceDecl(String name, @Nullable BaseType superClass, List<BaseType> interfaces, List<TSVariableType> variableTypes) {
         super(name, superClass, interfaces, variableTypes);
-        this.namespace = new Wrapped.Namespace(name);
+    }
+
+    public ClassDecl createStaticClass() {
+        val classDecl = new ClassDecl(
+            ImportType.STATIC.fmt(this.name),
+            null,
+            Collections.singletonList(Types.primitive(this.name)),
+            this.variableTypes
+        );
+        //methods will at the original interface
+        //classDecl.methods.addAll(methods);
+        classDecl.fields.addAll(fields);
+
+        return classDecl;
+    }
+
+    public Wrapped.Namespace createNamespace() {
+        val namespace = new Wrapped.Namespace(this.name);
+        for (val field : fields) {
+            // if (!field.isStatic) throw new RuntimeException("Why an interface can have a non-static field?");
+            // Because ProbeJS can add non-static fields to it... And it's legal in TypeScript.
+            namespace.addCode(field.asVariableDecl());
+        }
+        for (val method : methods) {
+            if (method.isStatic) {
+                namespace.addCode(method.asFunctionDecl());
+            }
+        }
+        // Adds a marker in it to prevent VSCode from not recognizing the namespace to import
+        if (namespace.isEmpty()) {
+            namespace.addCode(new VariableDeclaration("probejs$$marker", Types.NEVER));
+        }
+        return namespace;
     }
 
     @Override
@@ -29,11 +63,15 @@ public class InterfaceDecl extends ClassDecl {
         // Format head - export interface name<T> extends ... {
         String head = String.format("export interface %s", name);
         if (!variableTypes.isEmpty()) {
-            String variables = variableTypes.stream().map(type -> type.line(declaration, BaseType.FormatType.VARIABLE)).collect(Collectors.joining(", "));
+            String variables = variableTypes.stream()
+                .map(type -> type.line(declaration, BaseType.FormatType.VARIABLE))
+                .collect(Collectors.joining(", "));
             head = String.format("%s<%s>", head, variables);
         }
         if (!interfaces.isEmpty()) {
-            String formatted = interfaces.stream().map(type -> type.line(declaration)).collect(Collectors.joining(", "));
+            String formatted = interfaces.stream()
+                .map(type -> type.line(declaration))
+                .collect(Collectors.joining(", "));
             head = String.format("%s extends %s", head, formatted);
         }
         head = String.format("%s {", head);
@@ -41,23 +79,10 @@ public class InterfaceDecl extends ClassDecl {
         // Format body - fields, constructors, methods
         List<String> body = new ArrayList<>();
 
-        for (FieldDecl field : fields) {
-            // if (!field.isStatic) throw new RuntimeException("Why an interface can have a non-static field?");
-            // Because ProbeJS can add non-static fields to it... And it's legal in TypeScript.
-            namespace.addCode(field.asVariableDecl());
-        }
         body.add("");
         for (MethodDecl method : methods) {
-            if (method.isStatic) {
-                namespace.addCode(method.asFunctionDecl());
-            } else {
-                body.addAll(method.format(declaration));
-            }
-        }
-
-        // Adds a marker in it to prevent VSCode from not recognizing the namespace to import
-        if (namespace.isEmpty()) {
-            namespace.addCode(new VariableDeclaration("probejs$$marker", Types.NEVER));
+            //include static methods for StaticClass creation
+            body.addAll(method.format(declaration));
         }
 
         // tail - }
@@ -74,7 +99,8 @@ public class InterfaceDecl extends ClassDecl {
         formatted.addAll(tail);
 
         // Static methods and fields, adds it even if it's empty, so auto import can still discover it
-        formatted.addAll(namespace.format(declaration));
+        formatted.addAll(createNamespace().format(declaration));
+        formatted.addAll(createStaticClass().format(declaration));
         return formatted;
     }
 }
