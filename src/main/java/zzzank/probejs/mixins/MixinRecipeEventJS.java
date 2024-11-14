1a9f0a9db3e6b20a426d9b1d3708c572f3e7bf08
ZZZank <3410764033@qq.com>
2024-11-14 09:38:28 +0800

    fix compiling for mixin

--- src/main/java/zzzank/probejs/mixins/MixinRecipeEventJS.java
+++ src/main/java/zzzank/probejs/mixins/MixinRecipeEventJS.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonObject;
 import dev.latvian.kubejs.recipe.RecipeEventJS;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeManager;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,13 +18,14 @@ import java.util.Map;
  * @author ZZZank
  */
 @Mixin(value = RecipeEventJS.class, remap = false)
-public interface AccessRecipeEventJS {
+public abstract class MixinRecipeEventJS {
 
     @Shadow
-    Map<String, Object> getRecipes();
+    @Final
+    private Map<String, Object> recipeFunctions;
 
     @Inject(method = "post", at = @At("HEAD"))
-    default void captureSelf(RecipeManager recipeManager, Map<ResourceLocation, JsonObject> jsonMap, CallbackInfo ci) {
-        RecipeEvents.captureRecipes(this.getRecipes());
+    void captureSelf(RecipeManager recipeManager, Map<ResourceLocation, JsonObject> jsonMap, CallbackInfo ci) {
+        RecipeEvents.captureRecipes(this.recipeFunctions);
     }
 }
