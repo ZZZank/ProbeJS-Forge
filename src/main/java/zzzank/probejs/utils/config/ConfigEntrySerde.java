66736bcdeff51f5a2e68d12b1b6ddbab12d60b2b
ZZZank <3410764033@qq.com>
2024-12-04 10:59:07 +0800

    better type context for config

--- src/main/java/zzzank/probejs/utils/config/ConfigEntrySerde.java
+++ src/main/java/zzzank/probejs/utils/config/ConfigEntrySerde.java
@@ -1,17 +1,20 @@
 package zzzank.probejs.utils.config;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import com.mojang.datafixers.util.Pair;
 import lombok.AllArgsConstructor;
 import lombok.val;
+import org.jetbrains.annotations.NotNull;
 import zzzank.probejs.ProbeJS;
 import zzzank.probejs.utils.JsonUtils;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author ZZZank
@@ -28,26 +31,19 @@ public class ConfigEntrySerde {
     public ConfigEntry<?> fromJson(String jsonName, JsonObject jsonObject) {
         try {
             val name$namespace = jsonName.lastIndexOf('.');
-
             val name = name$namespace < 0 ? jsonName : jsonName.substring(name$namespace + 1);
-            val namespace = name$namespace < 0 ? null : jsonName.substring(0, name$namespace);
-            val defaultValue = JsonUtils.deserializeObject(jsonObject.get(DEFAULT_VALUE_KEY));
+            val namespace = name$namespace < 0 ? source.defaultNamespace : jsonName.substring(0, name$namespace);
+
+            val reference = source.get(namespace, name);
+            Function<JsonElement, Object> deserializer = reference == null
+                ? JsonUtils::deserializeObject
+                : o -> ProbeJS.GSON.fromJson(o, reference.defaultValue.getClass());
+
+            val defaultValue = deserializer.apply(jsonObject.get(DEFAULT_VALUE_KEY));
             val value = jsonObject.has(VALUE_KEY)
-                ? JsonUtils.deserializeObject(jsonObject.get(VALUE_KEY))
+                ? deserializer.apply(jsonObject.get(VALUE_KEY))
                 : defaultValue;
-            List<String> comments;
-            val jsonComments = jsonObject.get(COMMENTS_KEY);
-            if (jsonComments instanceof JsonPrimitive primitive) {
-                comments = Collections.singletonList(primitive.getAsString());
-            } else if (jsonComments instanceof JsonArray array) {
-                val l = new ArrayList<String>(array.size());
-                for (val element : array) {
-                    l.add(element.getAsString());
-                }
-                comments = l;
-            } else {
-                comments = Collections.emptyList();
-            }
+            val comments = extractComments(jsonObject);
 
             val entry = new ConfigEntry<>(source, name, defaultValue, namespace, comments);
             entry.setNoSave(value);
@@ -58,21 +54,43 @@ public class ConfigEntrySerde {
         return null;
     }
 
+    private static @NotNull List<String> extractComments(JsonObject jsonObject) {
+        val jsonComments = jsonObject.get(COMMENTS_KEY);
+        if (jsonComments instanceof JsonPrimitive primitive) {
+            return Collections.singletonList(primitive.getAsString());
+        } else if (jsonComments instanceof JsonArray array) {
+            val l = new ArrayList<String>(array.size());
+            for (val element : array) {
+                l.add(element.getAsString());
+            }
+            return l;
+        }
+        return Collections.emptyList();
+    }
+
+    private JsonElement valueToJson(ConfigEntry<?> entry, Object value) {
+        if (value instanceof Enum<?> e) {
+            return new JsonPrimitive(e.name());
+        }
+        return JsonUtils.parseObject(value);
+    }
+
     public Pair<String, JsonObject> toJson(ConfigEntry<?> entry) {
         val object = new JsonObject();
 
         val name = entry.namespace + '.' + entry.name;
 
-        val value = JsonUtils.parseObject(entry.getRaw());
+        val value = valueToJson(entry, entry.getRaw());
         object.add(VALUE_KEY, value);
 
         switch (entry.comments.size()) {
-            case 0 -> {}
+            case 0 -> {
+            }
             case 1 -> object.add(COMMENTS_KEY, new JsonPrimitive(entry.comments.get(0)));
             default -> object.add(COMMENTS_KEY, JsonUtils.parseObject(entry.comments));
         }
 
-        val defaultValue = JsonUtils.parseObject(entry.defaultValue);
+        val defaultValue = valueToJson(entry, entry.defaultValue);
         object.add(DEFAULT_VALUE_KEY, defaultValue);
 
         return new Pair<>(name, object);
