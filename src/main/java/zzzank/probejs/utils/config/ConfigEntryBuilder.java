be632fdcd12c01b3cba34c8fadfb5b78e5f74c44
ZZZank <3410764033@qq.com>
2024-12-19 22:33:06 +0800

    make use of binding in config builder

--- src/main/java/zzzank/probejs/utils/config/ConfigEntryBuilder.java
+++ src/main/java/zzzank/probejs/utils/config/ConfigEntryBuilder.java
@@ -5,6 +5,10 @@ import lombok.experimental.Accessors;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 import zzzank.probejs.utils.Asser;
+import zzzank.probejs.utils.config.binding.ConfigBinding;
+import zzzank.probejs.utils.config.binding.DefaultBinding;
+import zzzank.probejs.utils.config.binding.RangedBinding;
+import zzzank.probejs.utils.config.binding.ReadOnlyBinding;
 
 import javax.annotation.Nonnull;
 import java.util.*;
@@ -24,29 +28,40 @@ public class ConfigEntryBuilder<T> {
     public T defaultValue;
     public String namespace;
     public List<String> comments;
+    public ConfigBinding<T> binding;
 
     public ConfigEntryBuilder(@NotNull ConfigImpl config, @NotNull String name) {
         this.root = config;
         this.name = name;
     }
 
-    public <T_> ConfigEntryBuilder<T_> setDefault(Class<T_> type, T_ value) {
+    @SuppressWarnings("unchecked")
+    public <T_> ConfigEntryBuilder<T_> setDefault(Class<T_> type, ConfigBinding<T_> binding) {
+        Asser.t(type.isInstance(binding.getDefault()), "config default value must match expected type");
         val casted = (ConfigEntryBuilder<T_>) this;
         casted.expectedType = Asser.tNotNull(type, "config expected type");
-        casted.defaultValue = Asser.tNotNull(value, "config default value");
-        Asser.t(expectedType.isInstance(defaultValue), "config default value must match expected type");
+        casted.binding = Asser.tNotNull(binding, "config binding");
         return casted;
     }
 
+    private <T_> ConfigEntryBuilder<T_> setDefault(ConfigBinding<T_> binding) {
+        return setDefault(binding.clazzFromDefaultValue(), binding);
+    }
+
     public <T_> ConfigEntryBuilder<T_> setDefault(@Nonnull T_ defaultValue) {
-        Asser.tNotNull(defaultValue, "config default value");
-        Class<?> type;
-        if (defaultValue instanceof Enum<?> e) {
-            type = e.getDeclaringClass();
-        } else {
-            type = defaultValue.getClass();
-        }
-        return setDefault((Class<T_>) type, defaultValue);
+        return setDefault(new DefaultBinding<>(defaultValue, name));
+    }
+
+    public <T_> ConfigEntryBuilder<T_> readOnly(@Nonnull T_ defaultValue) {
+        return setDefault(new ReadOnlyBinding<>(defaultValue, name));
+    }
+
+    public <T_ extends Comparable<T_>> ConfigEntryBuilder<T_> setDefault(
+        @Nonnull T_ defaultValue,
+        @Nonnull T_ min,
+        @Nonnull T_ max
+    ) {
+        return setDefault(new RangedBinding<>(defaultValue, name, min, max));
     }
 
     public ConfigEntryBuilder<T> comment(String... comments) {
@@ -66,7 +81,7 @@ public class ConfigEntryBuilder<T> {
         if (comments == null) {
             comments = Collections.emptyList();
         }
-        assert expectedType.isInstance(defaultValue);
+        assert expectedType.isInstance(binding.getDefault());
         return this.root.merge(
             new ConfigEntry<>(this.root, namespace, name, expectedType, defaultValue, comments)
         );
--- src/main/java/zzzank/probejs/utils/config/binding/ConfigBinding.java
+++ src/main/java/zzzank/probejs/utils/config/binding/ConfigBinding.java
@@ -1,6 +1,8 @@
 package zzzank.probejs.utils.config.binding;
 
+import lombok.val;
 import org.jetbrains.annotations.NotNull;
+import zzzank.probejs.utils.Asser;
 import zzzank.probejs.utils.config.error.ConfigReport;
 
 /**
@@ -18,5 +20,12 @@ public interface ConfigBinding<T> {
 
     default ConfigReport reset() {
         return set(getDefault());
+    }
+
+    @SuppressWarnings("unchecked")
+    default  <T_> Class<T_> clazzFromDefaultValue() {
+        val def = getDefault();
+        Asser.tNotNull(def, "config default value");
+        return def instanceof Enum<?> e ? (Class<T_>) e.getDeclaringClass() : (Class<T_>) def.getClass();
     }
 }
