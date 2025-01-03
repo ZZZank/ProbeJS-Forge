02f49652391946a82c209f047ecf1eeb7598abf1
ZZZank <3410764033@qq.com>
2025-01-03 18:09:02 +0800

    more flexible TSVariableType, fix "default to" formatting

--- src/main/java/zzzank/probejs/lang/typescript/code/type/ts/TSVariableType.java
+++ src/main/java/zzzank/probejs/lang/typescript/code/type/ts/TSVariableType.java
@@ -10,21 +10,21 @@ import zzzank.probejs.lang.typescript.refer.ImportInfos;
 import javax.annotation.Nonnull;
 
 public class TSVariableType extends BaseType {
-    public final String symbol;
-    public final BaseType extendsType;
-    public final BaseType defaultTo;
+    public String symbol;
+    public BaseType extend;
+    public BaseType defaultTo;
 
-    public TSVariableType(String symbol, @Nullable BaseType extendsType, BaseType defaultTo) {
+    public TSVariableType(String symbol, @Nullable BaseType extend, BaseType defaultTo) {
         this.symbol = symbol;
-        this.extendsType = extendsType == Types.ANY ? null : extendsType;
+        this.extend = extend == Types.ANY ? null : extend;
         this.defaultTo = defaultTo == Types.ANY ? null : defaultTo;
     }
 
     @Override
     public ImportInfos getImportInfos(@Nonnull FormatType type) {
         val imports = ImportInfos.of();
-        if (extendsType != null) {
-            imports.addAll(extendsType.getImportInfos(type));
+        if (extend != null) {
+            imports.addAll(extend.getImportInfos(type));
         }
         if (defaultTo != null) {
             imports.addAll(defaultTo.getImportInfos(type));
@@ -40,11 +40,11 @@ public class TSVariableType extends BaseType {
         val builder = new StringBuilder();
         //name
         builder.append(symbol);
-        if (extendsType != null) {
-            builder.append(" extends ").append(extendsType.line(declaration, FormatType.RETURN));
+        if (extend != null) {
+            builder.append(" extends ").append(extend.line(declaration, FormatType.RETURN));
         }
         if (defaultTo != null) {
-            builder.append(" = ").append(defaultTo.format(declaration, FormatType.RETURN));
+            builder.append(" = ").append(defaultTo.line(declaration, FormatType.RETURN));
         }
         return builder.toString();
     }
